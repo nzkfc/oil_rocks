1e465258e8bd619af55b3b01b17f35df719b44e3
nzkfc <nzryancas@gmail.com>
2023-07-09 22:55:57 +1200

    Initial Upload

---
+++ hrs_props/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+this_is_a_map 'yes'
+
+description 'HRS BASE PROPS'
+
+files{
+	'stream/hrs_props.ytyp',
+	'stream/oil_rocks/prop_oil_rocks.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/hrs_props.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/oil_rocks/prop_oil_rocks.ytyp'
+
+
+dependency '/assetpacks'
